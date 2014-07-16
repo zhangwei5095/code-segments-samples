c60bc9d9cc8d848a4cb782ede3d29dd3c2885a01
playerlife <playerlife@163.com>
2014-07-16 10:01:58 +0800

    joejoe
    joejoe desc

---
+++ MyView.java
@@ -0,0 +1,20 @@
+package com.joejoe.mybitmaptest.bitmaptest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by joejoe on 2014/7/1.
+ */
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public  void onDraw(Canvas canvas) {
+
+    }
+
+}
